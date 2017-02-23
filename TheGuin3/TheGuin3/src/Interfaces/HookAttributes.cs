fc4a0f23c7e5032a1adb1c930a9125aed1fb8db6
Joshua Ashton <joshua@corruptedstudios.net>
2017-02-23 15:57:04 +0000

    Hooks

---
+++ TheGuin3/TheGuin3/src/Interfaces/HookAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGuin3.Interfaces
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnCommand : System.Attribute
+    {
+        public OnCommand(string name, string description, bool adminOnly)
+        {
+            Name = name;
+            Description = description;
+            AdminOnly = adminOnly;
+        }
+        public string Name;
+        public string Description;
+        public bool AdminOnly;
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnMessage : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnUserUnbanned : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnUserBanned : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnUserLeft : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnUserJoined : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class OnUserChange : System.Attribute
+    { }
+}
